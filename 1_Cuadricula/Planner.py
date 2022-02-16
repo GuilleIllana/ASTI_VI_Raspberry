e5216e10209135030b90b6f878ca93c4fafda166
Guillermo Illana Gisbert <47231110+GuilleIllana@users.noreply.github.com>
2022-02-17 00:18:17 +0100

    Cuadricula time

---
+++ 1_Cuadricula/Planner.py
@@ -0,0 +1,7 @@
+from Planning_algorithms import astar
+
+
+def Planner(graph, start, end):
+    #return bfs(graph, start, end)
+    return astar(graph, start, end)
+
---
+++ 1_Cuadricula/main.py
@@ -0,0 +1,26 @@
+from Planner import *
+
+def main():
+    # 0 = Free circulation node, 1 = Obstacle
+    graph = [[0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0]]
+
+    start = (0, 0)
+    end = (7, 6)
+
+    nodes = Planner(graph, start, end)
+    print(nodes)
+
+    for node in nodes:
+        print(node)
+
+if __name__ == '__main__':
+    main()
